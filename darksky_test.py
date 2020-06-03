9c3cc0fbd72c2ee98feb48c711c4a63093309a8b
Walker Sorlie <walkersorlie@gmail.com>
2020-06-03 16:56:57 -0700

    File for formatting eventual output

--- darksky_test.py
+++ darksky_test.py
@@ -2,12 +2,17 @@ import json
 import requests
 import os
 from datetime import datetime, timezone
+from pymongo import MongoClient
 
 
 
-def unix_time_to_local_time(unix_time):
-    utc_time = datetime.fromtimestamp(unix_time, timezone.utc)
+def unix_timestamp_to_local_time(unix_timestamp):
+    utc_time = datetime.fromtimestamp(unix_timestamp, timezone.utc)
     return utc_time.astimezone()
+
+def local_timestamp_to_utc_timestamp(local_timestamp):
+    utc_datetime = datetime.fromtimestamp(local_timestamp, timezone.utc)
+    return utc_datetime.astimezone(timezone.utc).replace(tzinfo=None).timestamp()
 
 
 if not os.path.isfile('darksky_test_info.json'):
@@ -27,49 +32,59 @@ if not os.path.isfile('darksky_test_info.json'):
 with open('darksky_test_info.json', 'r') as myfile:
     data = json.loads(myfile.read())
 
-# print(json.dumps(data['currently'], sort_keys=True, indent=2))
-# print(json.dumps(data['daily'], sort_keys=True, indent=2))
 
-print(json.dumps(data['currently'], indent=2))
-print("Feels like: %s °F" % json.dumps(data['currently']['apparentTemperature'], indent=2))
-# print(datetime.utcfromtimestamp(data['currently']['time']).strftime('%Y-%m-%d %H:%M:%S'))
-utc_time = datetime.fromtimestamp(data['currently']['time'], timezone.utc)
-# print(utc_time)
-# local_time = utc_time.astimezone()
-local_time = unix_time_to_local_time(data['currently']['time'])
-# print(local_time)
-print(local_time.strftime("%Y-%m-%d %H:%M:%S (%Z)"))
 
-"""
-Next three days (might include today as well)
-"""
-print(json.dumps(data['daily']['data'][0], indent=2))
-for x in range(3):
-    print("High temp: %s " % data['daily']['data'][x]['temperatureHigh'])
-    print("High temp time: %s " % unix_time_to_local_time(data['daily']['data'][x]['temperatureHighTime']).strftime("%Y-%m-%d %H:%M:%S (%Z)"))
-    print("Min temp: %s " % data['daily']['data'][x]['temperatureLow'])
-    print("Min temp time: %s " % unix_time_to_local_time(data['daily']['data'][x]['temperatureMinTime']).strftime("%Y-%m-%d %H:%M:%S (%Z)"))
-    print("Max temp: %s " % data['daily']['data'][x]['temperatureMax'])
-    print("Max temp time: %s " % unix_time_to_local_time(data['daily']['data'][x]['temperatureMaxTime']).strftime("%Y-%m-%d %H:%M:%S (%Z)"))
-    precip_probability = data['daily']['data'][x]['precipProbability'] * 100
-    print(f"Precip probability: {precip_probability}%")
-    print("Precip intensity (inches of water per hour): %s " % data['daily']['data'][x]['precipIntensity'])
-    if data['daily']['data'][x]['precipIntensity'] > 0:
-        print("Precip type: %s " % data['daily']['data'][x]['precipType'])
-    print("")
-    # print("Date: %s " % unix_time_to_local_time(data['daily']['data'][x]['time']).strftime("%Y-%m-%d %H:%M:%S (%Z)"))
+data_point_currently = data['currently']
+# print(json.dumps(data_point_currently, indent=2))
+# print(unix_timestamp_to_local_time(data_point_currently['time']).strftime("%Y-%m-%d %H:%M:%S (%Z)"))
+
+
+db_url = 'mongodb://localhost:27017'
+client = MongoClient(db_url)
+
+db = client.weather_server
+collection_currently = db.currently_weather_collection
+document_currently = data_point_currently
+document_id_currently = collection_currently.insert_one(document_currently).inserted_id
+# client.close()
+
 
 """
-Next 6 hours
+Next 6 days (includes today). Total of 7 days. Starts and ends at midnight
 """
-# for x in range(5):
-#     print(json.dumps(data['hourly']['data'][x], indent=2))
-#     time = data['hourly']['data'][x]['time']
-#     print(datetime.utcfromtimestamp(time).strftime('%Y-%m-%d %H:%M:%S'))
-#
-#     utc_time = datetime.fromtimestamp(time, timezone.utc)
-#     print(utc_time)
-#     local_time = utc_time.astimezone()
-#     print(local_time)
-#     print(local_time.strftime("%Y-%m-%d %H:%M:%S (%Z)"))
-#
+days_data_block = []
+for day in data['daily']['data']:
+    # print(datetime.utcfromtimestamp(day['time']).strftime("%Y-%m-%d %H:%M:%S (%Z)"))
+    # print(unix_timestamp_to_local_time(day['time']).strftime("%Y-%m-%d %H:%M:%S (%Z)"))
+
+    day['time'] = local_timestamp_to_utc_timestamp(day['time'])
+    days_data_block.append(day)
+
+
+collection_daily = db.collection_weather_daily
+# result = collection_daily.insert_many(next_three_days)
+
+dt = datetime.now().astimezone()
+# print(dt.strftime("%Y-%m-%d %H:%M:%S (%Z)"))
+dt = dt.replace(hour=0, minute=0, second=0, microsecond=0)
+# print(dt.strftime("%Y-%m-%d %H:%M:%S (%Z)"))
+# print(dt.timestamp())
+dt_utc = dt.astimezone(timezone.utc).replace(tzinfo=None)
+# print(dt_utc)
+# print(dt_utc.timestamp())
+
+# for doc in collection_daily.find({"time": {"$lt": dt_utc.timestamp()}}):
+#     print(unix_timestamp_to_local_time(doc['time']).strftime("%Y-%m-%d %H:%M:%S (%Z)"))
+
+
+"""
+The next two days, starting and ending on the hour the request was made
+"""
+hours_data_block = []
+for hour in data['hourly']['data']:
+    hour['time'] = local_timestamp_to_utc_timestamp(hour['time'])
+    hours_data_block.append(hour)
+
+
+collection_hourly = db.collection_weather_hourly
+# collection_hourly_result = collection_hourly.insert_many(hours_data_block)
