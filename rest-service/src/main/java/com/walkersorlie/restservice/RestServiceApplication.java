02c5be5a37ce84b41192ec9236dd96873d1b151f
Walker Sorlie <walkersorlie@gmail.com>
2020-08-20 15:38:06 -0700

    Refactor to package rest-service as a WAR file

--- rest-service/src/main/java/com/walkersorlie/restservice/RestServiceApplication.java
+++ rest-service/src/main/java/com/walkersorlie/restservice/RestServiceApplication.java
@@ -3,13 +3,20 @@ package com.walkersorlie.restservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class RestServiceApplication {
+public class RestServiceApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestServiceApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(RestServiceApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestServiceApplication.class, args);
+    }
 
 }
